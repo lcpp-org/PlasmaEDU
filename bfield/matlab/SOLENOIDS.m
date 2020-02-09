066820852135368a75e8dbf460ffa80a33e084f3
Davide Curreli <dcurreli@illinois.edu>
2020-02-09 12:10:36 -0600

    Added comments to SOLENOIDS.m

--- bfield/matlab/SOLENOIDS.m
+++ bfield/matlab/SOLENOIDS.m
@@ -7,25 +7,25 @@ global CurrentLoops
 % CurrentLoops = [  Xc,  Yc,  Zc,  nx,  ny,  nz,  I0,   Ra,   N_windings; ...
   CurrentLoops = [ -0.1, 0.0, 0.0, 1.0, 0.0, 0.0, 10e3, 0.025, 1; ...
                     0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 10e3, 0.05,  1; ... 
-                    0.1, 0.0, 0.0, 1.0, 0.0, 0.0,-10e3, 0.10,  1; ...
+                    0.1, 0.0, 0.0, 1.0, 0.0, 0.0, 10e3, 0.10,  1; ...
                     0.2, 0.0, 0.0, 1.0, 0.0, 0.0, 10e3, 0.05,  1; ...
                     0.3, 0.0, 0.0, 1.0, 0.0, 0.0, 10e3, 0.025, 1]; 
 
-% Points of the mapping plane
-Px =  -0.1 : 0.2/45  : 0.30 ;
-Py = 0.001 : 0.15/40 : 0.15 ;
-Pz = 0.0 ;
 
-% Let's plot everything in a single figure
-figureNum = 1;
-figure( figureNum )
+% Open a figure
+figureNumber = 1;
+figure( figureNumber )
 
-% Plot 3D of the current loops
-plot3D_currentloops( CurrentLoops, 100, figureNum )
-% plot3   ( P(1), P(2), P(3), 'ro')
-% quiver3 ( P(1), P(2), P(3), B(1), B(2), B(3) ) 
+% 3D plot of current loops
+plot3D_currentloops( CurrentLoops, 100, figureNumber )
 
-% Map of the B-field isolines
+% Grid of points for contour map of B-field isolines 
+Px =  -0.1 : 0.2/45  : 0.30;
+Py = 0.001 : 0.15/40 : 0.15;
+Pz = 0.0;
+
+% Contour map of B-field isolines
+Bnorm = zeros(size(Px,2),size(Py,2));
 for i=1:size(Px,2)
     for j=1:size(Py,2)
         P = [ Px(i); Py(j); Pz ];
@@ -37,7 +37,7 @@ end
 contour(XX',YY',Bnorm, 250)
 axis equal
 
-% B-field lines
+% Fieldlines
 % (Set: coordinates of the seed points, direction, length along 's', ds_maxstep  )
 Y0x       = [ -0.1*ones(1,11),  0.1*ones(1,11),  0.1*ones(1,11),  0.3*ones(1,11)  ];
 Y0y       = [  0.0:0.003:0.03,  0.0:0.006:0.06,  0.0:0.006:0.06,  0.0:0.003:0.03  ];
@@ -51,6 +51,8 @@ for i=1:numel(Y0y)
     plot3( y(:,1), y(:,2), y(:,3), 'k-' )
 end
 
+% Set a view 
 view([0 0 1])
-print( '-f1', '-dpdf', 'fig01' )
 
+% Print figure on file 
+print( '-f1', '-dpdf', 'fig01' )
