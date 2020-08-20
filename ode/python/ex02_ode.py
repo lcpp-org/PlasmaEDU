9b68f6b065daa4b7ca5ec28a3f832ab378831ef0
Davide Curreli <dcurreli@illinois.edu>
2020-08-20 09:56:49 -0500

    Ode Reference solution in ex02

--- ode/python/ex02_ode.py
+++ ode/python/ex02_ode.py
@@ -12,7 +12,7 @@ def main():
     y_ef = ode.euler( fun, xn, y0 )       # Forward Euler
     y_mp = ode.midpoint( fun, xn, y0 )    # Explicit Midpoint
     y_rk = ode.rk4( fun, xn, y0 )         # Runge-Kutta 4
-    y_an = np.exp(xn)                     # Analytical
+    y_an = xn**2 + 2.0*xn + 1.0 - 0.5*np.exp(xn) # Analytical                     # Analytical
 
     for i in range(0,xn.size):
         print xn[i], y_an[i], y_ef[i,0], y_mp[i,0], y_rk[i,0]
@@ -24,7 +24,7 @@ def main():
     plt.plot( xn, y_an, 'k-',  label='Analytical Solution' )
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.legend(loc=2)
+    plt.legend(loc=3)
     plt.savefig('ex02_ode_solution.png')
     plt.show()
 
