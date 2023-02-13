72e776bd6788db82dc60bdff0c2562e5bf37f5db
Davide Curreli <dcurreli@illinois.edu>
2023-02-12 18:01:52 -0600

    Create materials.py

---
+++ sputtering/materials.py
@@ -0,0 +1,284 @@
+titanium = {
+    'symbol': 'Ti',
+    'name': 'titanium',
+    'Z': 22,
+    'm': 47.867,
+    'Es': 4.84,
+    'Ec': 3.5,
+    'Eb': 3.,
+    'Q': 0.58,
+    'n': 5.67e28,
+    'W': 2.57,
+    's': 2.5
+}
+
+hydrogen = {
+    'symbol': 'H',
+    'name': 'hydrogen',
+    'Z': 1,
+    'm': 1.008,
+    'Ec': 0.95,
+    'Es': 1.5,
+}
+
+lithium = {
+    'symbol': 'Li',
+    'name': 'lithium',
+    'Z': 3,
+    'm': 6.941,
+    'Ec': 1.0,
+    'Es': 1.64,
+    'Eb': 0.0,
+    'n': 4.63E28
+}
+
+carbon = {
+    'symbol': 'C',
+    'name': 'carbon',
+    'Z': 6,
+    'm': 12.011,
+    'Ec': 1.0,
+    'Es': 1.5, 
+}
+
+nitrogen = {
+    'symbol': 'N',
+    'name': 'nitrogen',
+    'Z': 7,
+    'm': 14,
+    'n': 5.4E25,
+    'Es': 0.,
+    'Eb': 0.,
+    'Ec': 1.0,
+    'Q': 1.
+}
+
+deuterium = {
+    'symbol': 'D',
+    'name': 'deuterium',
+    'Z': 1,
+    'm': 2,
+    'Ec': 0.1,
+    'Es': 1.5,
+}
+
+helium = {
+    'symbol': 'He',
+    'name': 'helium',
+    'Z': 2,
+    'm': 4.002602,
+    'Ec': 1.0,
+    'Es': 0.
+}
+
+beryllium = {
+    'symbol': 'Be',
+    'name': 'beryllium',
+    'Z': 4,
+    'm': 9.012182,
+    'n': 1.2347E29,
+    'Es': 3.31,
+    'Eb': 0.,
+    'Ec': 3.,
+    'Q': 2.17,
+    'W': 2.32,
+    's': 2.5
+}
+
+uranium = {
+    'symbol': 'U',
+    'name': 'uranium',
+    'Z': 92,
+    'm': 237.048166,
+    'n': 5.195E28,
+    'Es': 5.19,
+    'Ec': 3.0,
+    'Eb': 0.0,
+    'Q': 1.0,
+}
+
+lead = {
+    'symbol': 'Pb',
+    'name': 'lead',
+    'Z': 82,
+    'm': 207.2,
+    'n': 3.299E28,
+    'Es': 2.03,
+    'Ec': 1.5,
+    'Eb': 2.03,
+    'Q': 1.0
+}
+
+boron = {
+    'symbol': 'B',
+    'name': 'boron',
+    'Z': 5,
+    'm': 10.811,
+    'n': 1.309E29,
+    'Es': 5.76,
+    'Eb': 0.,
+    'Ec': 5.,
+    'Q': 4.6,
+    'W': 4.39,
+    's': 2.5
+}
+
+arsenic = {
+    'symbol': 'As',
+    'name': 'arsenic',
+    'Z': 33,
+    'm': 74.921595,
+    'n': 4.603E28,
+    'Es': 3.12,
+    'Eb': 0.,
+    'Ec': 1.,
+    'Q': None,
+    'W': None,
+    's': None
+}
+
+neon = {
+    'symbol': 'Ne',
+    'name': 'neon',
+    'Z': 10,
+    'm': 20.1797,
+    'Ec': 1.0,
+    'Es': 0.
+}
+
+krypton = {
+    'symbol': 'Kr',
+    'name': 'krypton',
+    'Z': 36,
+    'm': 83.80,
+    'Ec': 1.0,
+    'Es': 0.
+}
+
+silicon = {
+    'symbol': 'Si',
+    'name': 'silicon',
+    'Z': 14,
+    'm': 28.08553,
+    'n': 4.90E28,
+    'Es': 4.72,
+    'Eb': 0.,
+    'Ec': 1.5,
+    'Q': 0.66,
+    'W': 2.32,
+    's': 2.5
+}
+
+argon = {
+    'symbol': 'Ar',
+    'name': 'argon',
+    'Z': 18,
+    'm': 39.948,
+    'Ec': 1.0,
+    'Es': 0.
+}
+
+oxygen = {
+    'symbol': 'O',
+    'name': 'oxygen',
+    'Z': 8,
+    'm': 15.9994,
+    'Eb': 2.58,
+    'Ec': 2.0,
+    'Es': 2.58,
+    'n': 4.291E28
+}
+
+aluminum = {
+    'symbol': 'Al',
+    'name': 'aluminum',
+    'Z': 13,
+    'm': 26.98,
+    'n': 6.022E28,
+    'Es': 3.39, #Eckstein (1991) p. 80
+    'Ec': 3.,
+    'Eb': 0.,
+    'Q': 1.09
+}
+
+ytterbium = {
+    'symbol': 'Yb',
+    'name': 'ytterbium',
+    'Z': 70,
+    'm': 173.04,
+    'n': 2.424E28,
+    'Eb': 1.58,
+    'Es': 1.58,
+    'Ec': 1.0,
+    'Q': 1.0
+}
+
+copper = {
+    'symbol': 'Cu',
+    'name': 'copper',
+    'Z': 29,
+    'm': 63.546,
+    'n': 8.491E28,
+    'Es': (4.65 + 4.26 + 4.62)/3., #Eckstein (1991) p. 80
+    'Eb': 0.0,
+    'Ec': 3.0,
+    'Q': 1.30,
+    'W': 2.14,
+    's': 2.5
+}
+
+tungsten = {
+    'symbol': 'W',
+    'name': 'tungsten',
+    'Z': 74,
+    'm': 183.84,
+    'n': 6.32E28,
+    'Es': 8.90, # 8.79
+    'Eb': 0.0, # 38.0/6.0,
+    'Ec': 1.0,
+    'Q': 0.72,
+    'W': 2.136,
+    's': 2.8
+}
+
+gold = {
+    'symbol': 'Au',
+    'name': 'gold',
+    'Z': 79,
+    'm': 196.97,
+    'n': 5.901E28,
+    'Es': 3.79,
+    'Eb': 3.,
+    'Ec': 3.79,
+    'Q': 1.04,
+}
+
+nickel = {
+    'symbol': 'Ni',
+    'name': 'nickel',
+    'Z': 28,
+    'm': 58.69,
+    'n': 9.14E28,
+    'Es': (5.61 + 5.11 + 5.55)/3., #Eckstein (1991) p. 80
+    'Eb': 0.,
+    'Ec': 3.,
+    'Q': 1.06,
+}
+
+cesium = {
+    'symbol': 'Cs',
+    'name': 'cesium',
+    'Z': 55,
+    'm': 132.905,
+    'Ec': 0.8,
+    'Es': 0.8,
+}
+
+xenon = {
+    'symbol': 'Xe',
+    'name': 'xenon',
+    'Z': 54,
+    'm': 131.293,
+    'Ec': 1.0,
+    'Es': 0.
+}
